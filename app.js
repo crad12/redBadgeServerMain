c85f6db7b6422007e4de8b9b131ff7bca69df0d7
Chad Rader <ChadsMacAir@Chads-MacBook-Air-2.local>
2019-03-20 16:32:25 -0400

    saved after clearing tables

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const user = require('./controllers/usercontroller');
 const comment = require('./controllers/commentcontroller');
 const bodyParser = require('body-parser');
 
-sequelize.sync({force:true});
+sequelize.sync();
 
 app.use(bodyParser.json());
 app.use(require('./middleware/headers'));
